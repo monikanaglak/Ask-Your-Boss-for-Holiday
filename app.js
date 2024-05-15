9ec5292e46630995f78276a2d81697c664f58b48
monika <monikanaglak@outlook.fr>
2024-05-15 13:12:28 +0200

    function creation calender, after click

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 let current_date = new Date();
 let current_month = current_date.getMonth();
 let current_month_days = current_date.getDate();
-console.log(current_month_days);
 let month_dom = document.querySelector(".month");
 let months = [
   "January",
@@ -31,23 +30,35 @@ const month = document.querySelector(".month");
 const previous = document.querySelector(".prev");
 const next = document.querySelector(".next");
 const monika = document.querySelector(".monika");
+//funkcja ktora za kazdym razem tworzy nowy kalendarz w zaleznosci ile jest dni miesiaca
+//poznac miesiac
+//zobaczyc ile ma dni w danym miesiacy
+//stworzyc kalendarz
+function creating_calender(current_month){
+    if (thirty_one_days.includes(months[current_month])) {
+        for (let i = 0; i <= 31; i++) {
+          let div = document.createElement("div");
+          div.classList.add("box");
+          div.innerText = i;
+          monika.appendChild(div);
+        }
+      } else {
+        let div = document.createElement("div");
+        div.classList.add("box");
+        div.innerText = i;
+        monika.appendChild(div);
+      }
+}
 window.onload = (event) => {
   month_dom.innerHTML = months[current_month];
-  if (thirty_one_days.includes(months[current_month])) {
-    for (let i = 0; i <= 31; i++) {
-      let div = document.createElement("div")
-      div.classList.add("box")
-      div.innerText = i;
-      monika.appendChild(div)
-    }
-  } else {
-  }
+  creating_calender(current_month)
   previous.addEventListener("click", () => {
     current_month++;
     if (current_month >= 12) {
       current_month = 0;
     }
     month_dom.innerHTML = months[current_month];
+    console.log(months[current_month])
   });
   next.addEventListener("click", () => {
     current_month--;
