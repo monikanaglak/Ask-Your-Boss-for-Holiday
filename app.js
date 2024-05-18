9562e3e2ad4bde9b5da1596a86c07f8d88970df7
monika <monikanaglak@outlook.fr>
2024-05-18 21:10:06 +0200

    css research

--- app.js
+++ app.js
@@ -29,55 +29,57 @@ let thirty_days = ["April", "June", "September", "November"];
 const month = document.querySelector(".month");
 const previous = document.querySelector(".prev");
 const next = document.querySelector(".next");
-const monika = document.querySelector(".monika");
+const calendar = document.querySelector(".calendar");
 
-function cleaning_calender(){
-  month_dom.innerHTML = " ";
+function changeColor(e){
+  e.target.classList.add("box-color")
 }
-function creating_calender(current_month){
-  monika.innerHTML = ""
-    if (thirty_one_days.includes(months[current_month])){
-        for (let i = 0; i <= 31; i++){
-          let div = document.createElement("div");
-          div.classList.add("box");
-          div.innerText = i;
-          monika.appendChild(div);
-        }}
-        else if(thirty_days.includes(months[current_month])){
-          for(let i = 0; i<=30; i++){
-            let div = document.createElement("div");
-            div.classList.add("box");
-            div.innerText = i;
-            monika.appendChild(div);
-          } 
-        
-      }
+function creating_calender(current_month) {
+  calendar.innerHTML = "";
+  if (thirty_one_days.includes(months[current_month])) {
+    for (let i = 0; i <= 31; i++) {
+      let div = document.createElement("div");
+      div.addEventListener("click",changeColor)
+      div.classList.add("box");
+      div.innerText = i;
+      calendar.appendChild(div);
     }
-  
+  } else if (thirty_days.includes(months[current_month])) {
+    for (let i = 0; i <= 30; i++) {
+      let div = document.createElement("div");
+      div.addEventListener("click",changeColor)
+      div.classList.add("box");
+      div.innerText = i;
+      calendar.appendChild(div);
+    }
+  } else {
+    for (let i = 0; i <= 28; i++) {
+      let div = document.createElement("div");
+      div.classList.add("box");
+      div.innerText = i;
+      calendar.appendChild(div);
+    }
+  }
+}
+
 window.onload = (event) => {
-  //zczytanie miesiaca i wstrzykniecie do dom
   month_dom.innerHTML = months[current_month];
-  creating_calender(current_month)
-  previous.addEventListener("click",()=>{
+  creating_calender(current_month);
+  next.addEventListener("click", () => {
     current_month++;
     if (current_month >= 12) {
       current_month = 0;
     }
     let next_month = current_month++;
-
-    month_dom.innerHTML = months[next_month]
-    creating_calender(next_month)
-  })
-  /*previous.addEventListener("click", () => {
-    current_month++;
-    
-    month_dom.innerHTML = months[current_month];
-  });*/
-  next.addEventListener("click", () => {
+    month_dom.innerHTML = months[next_month];
+    creating_calender(next_month);
+  });
+  previous.addEventListener("click", () => {
     current_month--;
     if (current_month < 0) {
       current_month = 11;
     }
     month_dom.innerHTML = months[current_month];
+    creating_calender(current_month);
   });
 };
